4687159678cc140730ae32e831e0820b74b76aa8
thomasMelisonProvisorio <thomasmelison@outlook.com>
2023-03-23 22:14:41 -0300

    Iniciando Os testes

---
+++ src/test/java/BarcaBugsTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class BarcaBugsTest {
+
+    // somente exemplo
+    @org.junit.jupiter.api.Test
+    void ocupaLugar() {
+    }
+    // somentte um exemplo
+    @org.junit.jupiter.api.Test
+    void testOcupaLugar() {
+    }
+}
